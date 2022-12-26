20bdc2b1c8750236121cca5c52eb5464d3960171
Mateus Palmeira <jmateusps16@gmail.com>
2022-12-26 08:13:28 -0300

    add dropdown list

--- organo/src/components/FormField/index.js
+++ organo/src/components/FormField/index.js
@@ -1,7 +1,19 @@
+import DropdownList from '../DropdownList'
 import TextField from '../TextField'
 import './FormField.css'
 
 const FormField = () => {
+
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+
     return (
         <section className='form-field'>
             <form>
@@ -9,6 +21,7 @@ const FormField = () => {
                 <TextField label="Nome" placeholder="Digite seu nome" />
                 <TextField label="Cargo" placeholder="Digite seu cargo" />
                 <TextField label="Imagem" placeholder="Digite o endereço da imagem" />
+                <DropdownList label="Times" itens={times} />
             </form>
         </section>
     )
