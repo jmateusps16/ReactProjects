a998a86e3caa4cd0fa93eba2746df9692acf7c94
Mateus Palmeira <jmateusps16@gmail.com>
2023-01-01 23:11:49 -0300

    ajuste para opção em branco ao iniciar

--- organo/src/components/DropdownList/index.js
+++ organo/src/components/DropdownList/index.js
@@ -5,6 +5,7 @@ const DropdownList = (props) => {
         <div className='dropdown-list'>
             <label>{props.label}</label>
             <select onChange={event => props.toTheChange(event.target.value)} required={props.requiredC} value={props.valueC}>
+                <option value=''>Selecione uma Opção</option>
                 {props.itens.map(item => <option key={item}>{item}</option>)}
             </select>
         </div>
